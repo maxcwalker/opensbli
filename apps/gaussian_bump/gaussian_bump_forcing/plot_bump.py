972a295b4f7cde8054efcd1aabe83ef840a73283
Max Walker <maxwalker97@hotmail.co.uk>
2023-07-14 16:07:21 +0100

    linux 14 July

--- apps/gaussian_bump/gaussian_bump_forcing/plot_bump.py
+++ apps/gaussian_bump/gaussian_bump_forcing/plot_bump.py
@@ -40,6 +40,8 @@ Lx = 400.0
 scale = 2.31669259
 
 
+
+
 D11 = read_dataset(group1, "D11_B0")
 x = read_dataset(group1, "x0_B0")
 y = read_dataset(group1, "x1_B0")
@@ -54,31 +56,48 @@ a = numpy.sqrt(1.4*p/rho)
 M = numpy.sqrt(u**2 + v**2)/a
 T = 1.4*(Minf**2)*p/rho
 
-plt.plot(x[0,:],v[0,:])
-plt.xlabel('x')
-plt.ylabel('v')
-plt.title('v velocity along the wall')
-plt.savefig('v_vel_along_wall.pdf')
-levelsT = 25 #np.linspace(min(T),max(T),1000)
-levelsP = 25 #np.linspace(min(P),max(P),1000)
-levelsU = 25 #np.linspace(0,max(u),1000)
 
+# plt.plot(x[0,:],v[0,:])
+# plt.xlabel('x')
+# plt.ylabel('v')
+# plt.title('v velocity along the wall')
+# plt.savefig('v_vel_along_wall.pdf')
+
+
+
+fig2, b1 = plt.subplots(1,1)
+sep = b1.contourf(x, y, u, levels= numpy.linspace(-0.2,0.05,40), cmap= cm.jet) 
+b1.set_title("u velocity contours to show the regions of separation")
+plt.xlabel('x')
+b1.set_ylabel("y")
+b1.set_ylim([0, 15])
+b1.set_aspect(12)
+# divider = make_axes_locatable(b1)
+# cax = divider.append_axes("right", size=1, pad=5)
+ubar = plt.colorbar(sep, ax=b1)
+ubar.set_label("u velocity [$ms^1$]" ) #rotation= 270
+fig2.savefig('Recirculation_zone.pdf')
+
+
+
+
+levels = 100
 fig, (ax1,ax2, ax3) = plt.subplots(3,1)
 fig.set_size_inches(18.5, 10.5)
-T = ax1.contourf(x, y, D11, levels = levelsT) 
+T = ax1.contourf(x, y, T, levels = levels, cmap=cm.jet) 
 ax1.set_title("Contours for flow over a Gaussian bump")
 plt.xlabel('x')
 ax1.set_ylabel("y")
 tbar = plt.colorbar(T, ax=ax1)
 tbar.set_label("Temperature [$^{\circ}$C]" ) #rotation= 270
 
-P = ax2.contourf(x, y, p, levels = levelsP)
+P = ax2.contourf(x, y, p, levels = levels, cmap=cm.jet)
 ax2.set_ylabel("y")
 Pbar = plt.colorbar(P, ax=ax2)
 Pbar.set_label("Pressure [Pa]" ) #rotation= 270
 plt.legend
 
-U = ax3.contourf(x, y, M, levels = levelsU)
+U = ax3.contourf(x, y, M, levels = levels, cmap=cm.jet)
 ax3.set_ylabel("y")
 ubar = plt.colorbar(U, ax=ax3)
 ubar.set_label("Mach Number") #X_Velocity [ms$^{-1}$]
@@ -88,13 +107,6 @@ plt.savefig('Gaussian_bump_contours.pdf')
 
 
 
-fig2, b1 = plt.subplots(1,1)
-x = b1.contourf(x, y, u, levels= numpy.linspace(-0.2,0.05,40), cmap=cm.jet) 
-b1.set_title("u velocity contours to show the regions of separation")
-plt.xlabel('x')
-b1.set_ylabel("y")
-ubar = plt.colorbar(x, ax=b1)
-ubar.set_label("u velocity [$ms^1$]" ) #rotation= 270
 
 
 
